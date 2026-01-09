a9ec6aaf83c648a0606fb06688f9677c86642567
mehul1110 <mehulashra1110@gmail.com>
2026-01-09 11:35:41 +0530

    level1

---
+++ programming elements/Question2.java
@@ -0,0 +1,9 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int math = 94;
+        int physics = 95;
+        int chemistry = 96;
+        double avg = (math + physics + chemistry) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + avg);
+    }
+}
---
+++ programming elements/Question7.java
@@ -0,0 +1,11 @@
+public class Question7 {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double pi = Math.PI;
+        double volumeKm3 = (4.0 / 3.0) * pi * Math.pow(radiusKm, 3);
+        double kmToMiles = 1.0 / 1.6; // 1 km = 1/1.6 miles
+        double radiusMiles = radiusKm * kmToMiles;
+        double volumeMiles3 = (4.0 / 3.0) * pi * Math.pow(radiusMiles, 3);
+        System.out.println("The volume of earth in cubic kilometers is " + volumeKm3 + " and cubic miles is " + volumeMiles3);
+    }
+}
